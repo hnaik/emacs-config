a86d02d2b05917b66ebb6a4f157ef6a57354c32b
Harish Naik <harishgnaik@gmail.com>
2019-09-30 21:40:37 -0500

    Added elpa files

---
+++ elpa/ms-python-20190413.516/ms-python-autoloads.el
@@ -0,0 +1,22 @@
+;;; ms-python-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ms-python" "ms-python.el" (0 0 0 0))
+;;; Generated autoloads from ms-python.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ms-python" '("ms-python-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ms-python-autoloads.el ends here
---
+++ elpa/ms-python-20190413.516/ms-python-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ms-python" "20190413.516" "A lsp client for microsoft python language server." '((emacs "26.1") (lsp-mode "5.0")) :commit "d95ac8f1633764cdb4bb43bf44c2eb6b225f9f93" :keywords '("tools") :authors '(("Yong Cheng" . "xhcoding@163.com")) :maintainer '("Yong Cheng" . "xhcoding@163.com") :url "https://github.com/xhcoding/ms-python")
